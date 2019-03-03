dd0ab7666b582bb08809fc30863ab7648d36ab11
zhaoa1262 <league@WTS-IM-6.attlocal.net>
2019-03-02 17:27:40 -0800

    day8

--- src/elseif/AreYouHappy.java
+++ src/elseif/AreYouHappy.java
@@ -11,7 +11,9 @@ public static void main(String[] args) {
 	}
 	
 	else if (happy.equals("no")) {
+		
 		String want = JOptionPane.showInputDialog("Do you want to be happy?");
+		
 		if (want.equals("yes")) {
 			JOptionPane.showMessageDialog(null, "Change Something!");
 		}
